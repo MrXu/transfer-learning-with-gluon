8177489d53c856b6d4126796a1676bbc3d65ed6a
mrxu <lawist2010@gmail.com>
2018-01-07 13:45:36 +0800

    initial commit

---
+++ generate_rec.sh
@@ -0,0 +1,4 @@
+python im2rec.py ./data/train . --center-crop True --resize 224 --pack-label True
+python im2rec.py ./data/validation . --center-crop True --resize 224 --pack-label True
+python im2rec.py ./data/test . --center-crop True --resize 224 --pack-label True
+python im2rec.py ./data/sample . --center-crop True --resize 224 --pack-label True
